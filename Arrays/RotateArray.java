498b62847972e3e840aec003e218dd212c694c3b
sejaljangra <sejaljangra55@gmail.com>
2026-09-21 15:15:12 +0530

    Add RotateArray class to rotate an array

---
+++ Arrays/RotateArray.java
@@ -0,0 +1,17 @@
+public class RotateArray {
+    public static void main(String[] args) {
+
+        int[] arr = {10, 20, 30, 40, 50};
+
+        int first = arr[0];
+
+        for (int i = 0; i < arr.length - 1; i++) {
+            arr[i] = arr[i + 1];
+        }
+
+        arr[arr.length - 1] = first;
+
+        for (int num : arr)
+            System.out.print(num + " ");
+    }
+}
